aeb70c992af90b6e9275ed8d206d72d72aa1b6bc
Anistea <43377371+Anistea@users.noreply.github.com>
2020-03-05 16:58:21 +0100

    SEO resolution

--- index.php
+++ index.php
@@ -4,27 +4,34 @@
 
 <!DOCTYPE HTML>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title><?php print titre ?></title>
-	
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-	
-	<link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Author: ANIS AMDOUNI, 
+    Illustrator: Militello, Category: PHP projet, Price: $..., 
+    Length: ... pages">
+    <title><?php print titre ?></title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
-	<ul>
-		<li><a href="index.php?page=1">&lt;/&gt;  L'école</a></li>
-		<li><a href="index.php?page=2">&lt;/&gt; Formations</a></li>
-		<li><a href="index.php?page=3">&lt;/&gt;  Contact</a></li>
-		
-	</ul>
-	<main>
-		<h1><?php print titre ?></h1>
+    <ul>
+        <li><a href="index.php?page=1">&lt;/&gt; L'école</a></li>
+        <li><a href="index.php?page=2">&lt;/&gt; Formations</a></li>
+        <li><a href="index.php?page=3">&lt;/&gt; Contact</a></li>
 
-	
-			<?php
+    </ul>
+    <main>
+        <h1><?php print titre ?></h1>
+
+
+        <?php
 				if(isset($_GET['page'])){
 				//variable pour $_GET
 				$root = $_GET['page'];
@@ -38,8 +45,9 @@
 							include_once("./template/contact.html");
 						}
 						else if($root>3 || $root==0){
-							echo '<p class="warning"><i class="far fa-frown"></i> La page demandée n\'existe pas!!</p>';
-							//header("HTTP/1.0 404 Not Found");
+                            include_once("./404page.html");
+							//echo '<p class="warning"><i class="far fa-frown"></i> La page demandée n\'existe pas!!</p>';
+							header("HTTP/2.0 404 Not Found");
 						}
 
 				}
@@ -48,6 +56,7 @@
 					}
 
 			?>
-	</div>
+    </main>
 </body>
+
 </html>
